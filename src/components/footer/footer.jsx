7561756ef85354a9eb0267bb037492f4171831ea
phlybi <plbinh77@gmail.com>
2025-02-27 14:43:58 +0700

    add scss

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import './index.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <h3>Let's talk</h3>
+        
+        </header>
+    );
+}
+
+export default Header;
